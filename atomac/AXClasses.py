9597d7dc1f0ca8debc9c34cef44e70ed4a946bb2
陈旭锐 <cxr5@meitu.com>
2025-10-18 23:40:09 +0800

    【Bugfix】解决定位控件时出现KeyError: -25200的错误

--- atomac/AXClasses.py
+++ atomac/AXClasses.py
@@ -668,7 +668,7 @@ class BaseAXUIElement(_a11y.AXUIElement):
             target = self
         try:
             children = target.AXChildren
-        except _a11y.Error:
+        except Exception:
             return
         if children:
             for child in children:
@@ -689,7 +689,7 @@ class BaseAXUIElement(_a11y.AXUIElement):
         for k in kwargs.keys():
             try:
                 val = getattr(self, k)
-            except _a11y.Error:
+            except Exception:
                 return False
             # Not all values may be strings (e.g. size, position)
             if isinstance(val, str):
